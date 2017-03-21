a3e2d32c9784b264087ff5df687cbfa41b8fdcd6
tanmq <623881096@qq.com>
2017-03-21 16:25:39 +0800

    完善重连逻辑

--- common/tcpnode/nodeclient.go
+++ common/tcpnode/nodeclient.go
@@ -4,46 +4,82 @@ import (
 	"time"
 	"net"
 	"fmt"
+	"sync"
 
 	"linkServer/packet"
 	"linkServer/common/util"
 )
 
 
-const WAIT_FRESHOLD_TIME = 60
+const WAIT_FRESHOLD_TIME_SEC = 600
 
 
 type NodeClient struct {
-	receiver 	MessageReceive
+	receiver 	MessageReceiver
 
 	maxmessagelength  uint32
 	address						string
 	conn	  					net.Conn
-	connected					bool
+
+	mu								sync.Mutex
+	connected					bool 			// protected by mu
+	canceled					bool  		// protected by mu
 }
 
 
-func NewNodeClient(address string ,sender MessageSender, receiver MessageReceiver) *NodeClient {
+func NewNodeClient(address string ,receiver MessageReceiver) *NodeClient {
 	client := new(NodeClient)
 
-	client.sender						 	= sender
 	client.receiver				 		= receiver
 	client.address						= address
 	client.connected					= false
+	client.canceled						= false
 
 	client.connect()
 
 	return client
 }
 
-func (c *NodeClient)IsValid() {
+func (c *NodeClient)IsValid()  bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	return c.connected
+}
+
+func (c *NodeClient)valid() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.connected = true
+}
+
+func (c *NodeClient)unValid() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.conneced = false
+}
+
+func (c *NodeClient)IsCancel() bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.canceled
+}
+
+func (c *NodeClient)Cancel() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.canceled = true
+	c.conneced = false
+	c.conn.Close()
 }
 
 
 //reconnect the tcp connection.
 func (c *NodeClient)Reset() {
-	c.conneced = false
+	c.unValid()
 	if c.conn != nil { // close old connection
 		c.conn.Close()
 		fmt.Println("We are closing the old connection and begin to reconnect for address : ", c.address)
@@ -51,20 +87,23 @@ func (c *NodeClient)Reset() {
 	connect()
 }
 
+/**
+* This method will block util the connection is built or the client is canceled.
+*/
 func (c *NodeClient)connect() {
 
 	waitTime := 1
-	for !connected {
+	for !c.IsValid() && !c.IsCancel() {
 		c.conn, err = net.Dial("tcp", c.address)
 		if err == nil {
 			c.conn.SetWriteDeadline(time.Now().Add(time.Second))
-			c.connected = true
+			c.valid()
 			go c.handleReceive()
 			break
 		}
 
-		if waitTime >= MAX_EXPIRE_SEC {
-			waitTime = MAX_EXPIRE_SEC
+		if waitTime >= WAIT_FRESHOLD_TIME_SEC {
+			waitTime = WAIT_FRESHOLD_TIME_SEC
 		} else {
 			waitTime = 2 * waitTime
 		}
@@ -96,10 +135,11 @@ func (c *NodeClient)Send(data byte[]) error {
 
 
 func (c *NodeClient)handleReceive() {
-	for c.connected {
-		data, err := ioutil.FixedLengthRead(c.conn)
+	for c.Isvalid() {
+		data, err := util.FixedLengthRead(c.conn)
 		if err != nil {
-			fmt.Println("Wrong to read data. And we will receive data wait some time.")
+			fmt.Println("Wrong to read data.", err)
+			continue
 		}
 
 		receiver.OnMessage(data)
